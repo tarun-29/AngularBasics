d6946010cc9f383c71c73ae8438650c4a0d78b86
Tarun Kantiwal <29kantiwaltarun@gmail.com>
2020-06-04 23:22:46 +0530

    Route parameter

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,24 @@ import { Routes, RouterModule } from '@angular/router';
 import { DepartmentListComponent } from './department-list/department-list.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
+import { DeparmentDetailComponent } from './deparment-detail/deparment-detail.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'departments', pathMatch: 'full'},
-  {path: 'departments',component: DepartmentListComponent},
-  {path: 'employees', component: EmployeeListComponent},
-  {path: '**', component : PageNotFoundComponent}
+  { path: '', redirectTo: 'departments', pathMatch: 'full' },
+  { path: 'departments', component: DepartmentListComponent },
+  {path: 'departments/:id', component: DeparmentDetailComponent},
+  { path: 'employees', component: EmployeeListComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
-export const routingComponent = [DepartmentListComponent, EmployeeListComponent, PageNotFoundComponent]
-
+export class AppRoutingModule {}
+export const routingComponent = [
+  DepartmentListComponent,
+  EmployeeListComponent,
+  PageNotFoundComponent,
+  DeparmentDetailComponent
+];
--- src/app/department-list/department-list.component.ts
+++ src/app/department-list/department-list.component.ts
@@ -1,20 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-department-list',
   template: `
-    <p>
-      department-list works!
-    </p>
+    <h3>
+      Department List
+    </h3>
+    <ul class="items">
+      <li (click)="onSelect(department)" *ngFor="let department of departments">
+        <span class="badge">{{ department.id }}</span
+        >{{ department.name }}
+      </li>
+    </ul>
   `,
-  styles: [
-  ]
+  styles: []
 })
 export class DepartmentListComponent implements OnInit {
+  departments = [
+    { id: 1, name: 'Angular' },
+    { id: 2, name: 'Node' },
+    { id: 3, name: 'mongoDB' },
+    { id: 4, name: 'Ruby' },
+    { id: 5, name: 'Bootstrap' }
+  ];
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onSelect(department) {
+    this.router.navigate(['/departments', department.id]);
   }
-
 }
