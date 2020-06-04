4b325bbb21fb6e244d226aa0ce70dbc24d8a50cc
Tarun Kantiwal <29kantiwaltarun@gmail.com>
2020-06-05 00:02:42 +0530

    relativeNavigation

--- src/app/deparment-detail/deparment-detail.component.ts
+++ src/app/deparment-detail/deparment-detail.component.ts
@@ -33,6 +33,6 @@ export class DeparmentDetailComponent implements OnInit {
   }
   gotoDepartments(){
     let selectedId = this.departmentId ? this.departmentId : null;
-    this.router.navigate(['/departments',{id: selectedId}])
+    this.router.navigate(['../',{id: selectedId}],{relativeTo: this.route})
   }
 }
