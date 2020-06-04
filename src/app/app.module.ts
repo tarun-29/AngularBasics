02c52f6316f1d342f187fe6cc3de5a4b5509b5d1
Tarun Kantiwal <29kantiwaltarun@gmail.com>
2020-06-04 22:51:08 +0530

    Redirect routing and wildCard routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponent
+    routingComponent,
   ],
   imports: [
     BrowserModule,
